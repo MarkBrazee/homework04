4b40be00c6cd0e24904b73965351543e2078d9a5
Mark Brazee <m.brazee@comcast.net>
2020-09-29 15:59:50 -0600

    continuing js work

--- Assets/script.js
+++ Assets/script.js
@@ -8,13 +8,16 @@
     // A 'View Highscore' button appears in the upper left hand corner of the page
 
 // WHEN I click the start button
+        //addEventListner ("click")
 // THEN a timer starts and I am presented with a question
-    // A new page appears with multiple choice questions, each with a button. 
+    // A new page appears with multiple choice questions, each with a button.
     // The timer begins to count down.
 
 // WHEN I answer a question
     // When a button is selected a message indicating whether it was answred as correct or incorrect.
+        //addEventListner ("click")
     // Create a way to track the correct scores.
+
 
 // (Create a loop) THEN I am presented with another question.
 
@@ -28,20 +31,69 @@
     // a new page will appear 
         // showing your highscore and your initials
         // provide a Go Back button and Clear High Score button
-//_____________________________________________
+//==========================================================================================
 
 // Create variables
     // Array of questions for the quiz game
 
-var questionOne = [ 
+var questions = [ 
     
 ]
 
-var questionOneAnswr = [
+// Array of answers for 
+var answers = [
 
 
 ]
 
+// Start page, with instructions and a start button
 
 
+// A timer appears in the upper right hand corner of the page
+
+
+// A 'View Highscore' button appears in the upper left hand corner of the page
+
+
+// WHEN I click the start button
+        //addEventListner ("click")
+
+
+// The timer begins to count down.
+
+
+
+// A new page appears with multiple choice questions, each with a button.
+
+
+
+// When a button is selected a message indicating whether it was answred as correct or incorrect.
+        //addEventListner ("click")
+
+
+
+//Track score
 var score = 0;
+
+// (Create a loop) THEN I am presented with another question.
+
+
+
+
+
+// When all of the questions are answered or when the timer reaches zero.
+    // A new page will appear 
+        // A sentence will indicate you are All Done
+        // A sentence will appear with your score
+        // A input box will appear asking for your initials with a submit box next to it
+            //addEventListner ("click")
+
+
+
+
+
+// THEN the game is over
+    // a new page will appear 
+        // showing your highscore and your initials
+        // provide a Go Back button and Clear High Score button
+            //addEventListner ("click")
