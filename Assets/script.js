6e8e628c1f7384efb611e9ba96d3c3f1f4926614
Mark Brazee <m.brazee@comcast.net>
2020-09-29 09:34:31 -0600

    psuedo code in progress

--- Assets/script.js
+++ Assets/script.js
@@ -1,12 +1,28 @@
 // GIVEN I am taking a code quiz
+    // Need to develop a start page, with instructions and a start button
+    // A timer appears in the upper right hand corner of the page
+    // A 'View Highscore' button appears in the upper left hand corner of the page
+
 // WHEN I click the start button
 // THEN a timer starts and I am presented with a question
+    // A new page appears with multiple choice questions, each with a button. 
+    // The timer begins to count down.
+
 // WHEN I answer a question
-// THEN I am presented with another question
-// WHEN I answer a question incorrectly
-// THEN time is subtracted from the clock
-// WHEN all questions are answered or the timer reaches 0
+    // When a button is selected a message indicating whether it was answred as correct or incorrect.
+    // Create a way to track the correct scores.
+
+// (Create a loop) THEN I am presented with     another question.
+
+// When all of the questions are answered or when the timer reaches zero.
+    // A new page will appear 
+        // A sentence will indicate you are All Done
+        // A sentence will appear with your score
+        // A input box will appear asking for your initials with a submit box next to it
+
 // THEN the game is over
-// WHEN the game is over
+    // a new page will appear 
+        // showing your highscore and your initials
+        // 
 // THEN I can save my initials and score
 
